33c8c51abd1c1cdfc15ce3d4f02f4b8d93f09a50
Piotr Nowicki <piotrek24se@gmail.com>
2018-10-27 11:17:34 +0200

    Figures changing

--- src/Figures/Figures.java
+++ src/Figures/Figures.java
@@ -46,17 +46,41 @@ public class Figures {
 
         // zadanie 4 (trojkat prostokatny o podstawie 10 - *)
 
-        System.out.println("Zadanie 2 - kwadrat");
+        System.out.println("Zadanie 4 - trojkat prostokatny o podstawie 10");
+        int length = 10;
+        int counter = length + 1;
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+            counter--;
+            for (int j = 0; j < counter; j++) {
                 System.out.print("*" + " ");
             }
             System.out.println("");
         }
         System.out.println("-----------------------");
+
+        // zadanie 5 (trojkat rownoramienny o podstawie 11 - *) - dokonczyć (nie dziala prawidlowo)
+
+        System.out.println("Zadanie 5 - trojkat rownoramienny o podstawie 11");
+        int length1 = 11;
+        int counter2 = length1 + 1 / 2;
+        for (int i = 0; i < 11; i++) {
+            if (i == 10) {
+                for (int j = 0; j < 11; j++) {
+                    System.out.print("*" + " ");
+                }
+            } else {
+                for (int j = 0; j < 11; j++) {
+                    if (j < counter2 - i || j > counter2 + i) {
+                        System.out.print(" " + " ");
+                    } else System.out.print("*" + " ");
+                }
+            }
+
+            System.out.println("");
+
+        }
+        System.out.println("-----------------------");
     }
-
-
 
 
 }

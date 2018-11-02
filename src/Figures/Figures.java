4e9e793af634023f60d4a11d3504c036e791bfa0
Piotr Nowicki <piotrek24se@gmail.com>
2018-11-02 18:30:56 +0100

    Uzupelnienie zadania z trojkatem rownoramiennym

--- src/Figures/Figures.java
+++ src/Figures/Figures.java
@@ -58,18 +58,18 @@ public class Figures {
         }
         System.out.println("-----------------------");
 
-        // zadanie 5 (trojkat rownoramienny o podstawie 11 - *) - dokonczyć (nie dziala prawidlowo)
+        // zadanie 5 (trojkat rownoramienny o podstawie 11 - *)
 
         System.out.println("Zadanie 5 - trojkat rownoramienny o podstawie 11");
         int length1 = 11;
-        int counter2 = length1 + 1 / 2;
-        for (int i = 0; i < 11; i++) {
-            if (i == 10) {
-                for (int j = 0; j < 11; j++) {
+        int counter2 = (length1 + 1) / 2;
+        for (int i = 0; i < counter2; i++) {
+            if (i == counter2 - 1) {
+                for (int j = 0; j < length1; j++) {
                     System.out.print("*" + " ");
                 }
             } else {
-                for (int j = 0; j < 11; j++) {
+                for (int j = 1; j < length1; j++) {
                     if (j < counter2 - i || j > counter2 + i) {
                         System.out.print(" " + " ");
                     } else System.out.print("*" + " ");
